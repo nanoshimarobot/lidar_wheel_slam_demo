c2e2e4e203f078ed4c1433547724fd2127f8afa2
nanoshimarobot <shimada.toyozo.lf@tut.jp>
2025-12-30 23:49:58 +0900

    [refactor]

--- include/lidar_wheel_slam_demo/lidar_wheel_slam_demo_component.hpp
+++ include/lidar_wheel_slam_demo/lidar_wheel_slam_demo_component.hpp
@@ -1,194 +1,3 @@
-// #pragma once
-
-// #include <rclcpp/rclcpp.hpp>
-// #include <message_filters/subscriber.hpp>
-// #include <message_filters/sync_policies/approximate_time.h>
-// #include <message_filters/time_synchronizer.h>
-// #include <sensor_msgs/msg/point_cloud2.hpp>
-// #include <geometry_msgs/msg/pose.hpp>
-// #include <geometry_msgs/msg/pose_stamped.hpp>
-// #include <nav_msgs/msg/path.hpp>
-
-// #include <gtsam_points/types/point_cloud.hpp>
-// #include <gtsam_points/types/point_cloud_cpu.hpp>
-// #include <gtsam_points/factors/integrated_icp_factor.hpp>
-
-// #include <gtsam/nonlinear/NonlinearFactorGraph.h>
-// #include <gtsam/nonlinear/Values.h>
-// #include <gtsam/nonlinear/ISAM2.h>
-// #include <gtsam/geometry/Pose3.h>
-// #include <gtsam/slam/PriorFactor.h>
-// #include <gtsam/slam/BetweenFactor.h>
-// #include <gtsam/inference/Symbol.h>
-
-// #include <pcl/point_cloud.h>
-// #include <pcl/point_types.h>
-// #include <pcl_conversions/pcl_conversions.h>
-
-// #include <memory>
-
-// #include <Eigen/Core>
-// #include <Eigen/Geometry>
-
-// namespace c107 {
-
-// struct Frame {
-//   using Ptr = std::shared_ptr<Frame>;
-//   gtsam_points::PointCloudCPU::Ptr cloud;
-//   Eigen::Isometry3d T_world_robot;
-// };
-
-// using SyncPolicy = message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::PointCloud2, geometry_msgs::msg::PoseStamped>;
-// using Sync = message_filters::Synchronizer<SyncPolicy>;
-
-// using gtsam::symbol_shorthand::X;  // Pose3
-// class LidarWheelSLAMDemo : public rclcpp::Node {
-// private:
-
-//   rclcpp::TimerBase::SharedPtr timer_;
-//   rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
-
-//   std::shared_ptr<Sync> sync_ptr_;
-//   message_filters::Subscriber<sensor_msgs::msg::PointCloud2> point_cloud_sub_;
-//   message_filters::Subscriber<geometry_msgs::msg::PoseStamped> wheel_odom_sub_;
-
-//   Eigen::Isometry3d cr_wheel_T_world_robot_;
-//   Eigen::Isometry3d pr_wheel_T_world_robot_;
-
-//   std::unique_ptr<gtsam::ISAM2> isam2_;
-
-//   std::vector<Frame::Ptr> frames_;
-
-//   bool approx_zero(const double val, const double epsilon = 1e-6) { return std::fabs(val) < epsilon; }
-
-//   gtsam_points::PointCloudCPU::Ptr ros_cloud_convert(const sensor_msgs::msg::PointCloud2::SharedPtr pc_msg) {
-//     pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
-//     pcl::fromROSMsg(*pc_msg, *pcl_cloud);
-//     std::vector<Eigen::Vector4d> points;
-//     points.resize(pcl_cloud->points.size());
-//     for (size_t i = 0; i < pcl_cloud->points.size(); i++) {
-//       const auto& pcl_point = pcl_cloud->points[i];
-//       points[i] = Eigen::Vector4d(pcl_point.x, pcl_point.y, pcl_point.z, 1.0);
-//     }
-//     gtsam_points::PointCloudCPU::Ptr cloud = std::make_shared<gtsam_points::PointCloudCPU>(points);
-//     return cloud;
-//   }
-
-// public:
-//   LidarWheelSLAMDemo(const rclcpp::NodeOptions& options) : LidarWheelSLAMDemo("", options) {}
-//   LidarWheelSLAMDemo(const std::string& name_space = "", const rclcpp::NodeOptions& options = rclcpp::NodeOptions()) : Node("lidar_wheel_odom_demo", name_space, options) {
-//     path_pub_ = this->create_publisher<nav_msgs::msg::Path>("lidar_wheel_odom_demo/path", 10);
-//     point_cloud_sub_.subscribe(this, "/velodyne_points", rclcpp::SensorDataQoS().get_rmw_qos_profile());
-//     wheel_odom_sub_.subscribe(this, "/pose", rclcpp::SensorDataQoS().get_rmw_qos_profile());
-//     sync_ptr_ = std::make_shared<Sync>(SyncPolicy(10), point_cloud_sub_, wheel_odom_sub_);
-//     sync_ptr_->registerCallback(&LidarWheelSLAMDemo::sensor_data_cb, this);
-
-//     timer_ = this->create_wall_timer(
-//       std::chrono::milliseconds(100),
-//       std::bind(&LidarWheelSLAMDemo::timer_cb, this));
-//   }
-
-//   void timer_cb(){
-//     if(frames_.empty()) return;
-//     nav_msgs::msg::Path path_msg;
-//     path_msg.header.frame_id = "world";
-//     path_msg.header.stamp = this->now();
-//     for(size_t i=0; i<frames_.size(); i++){
-//       geometry_msgs::msg::PoseStamped pose_stamped;
-//       pose_stamped.header.frame_id = "world";
-//       pose_stamped.header.stamp = this->now();
-//       Eigen::Isometry3d T_world_robot = frames_[i]->T_world_robot;
-//       Eigen::Quaterniond q_world_robot(T_world_robot.linear());
-//       pose_stamped.pose.position.x = T_world_robot.translation().x();
-//       pose_stamped.pose.position.y = T_world_robot.translation().y();
-//       pose_stamped.pose.position.z = T_world_robot.translation().z();
-//       pose_stamped.pose.orientation.x = q_world_robot.x();
-//       pose_stamped.pose.orientation.y = q_world_robot.y();
-//       pose_stamped.pose.orientation.z = q_world_robot.z();
-//       pose_stamped.pose.orientation.w = q_world_robot.w();
-//       path_msg.poses.emplace_back(pose_stamped);
-//     }
-//     path_pub_->publish(path_msg);
-//   }
-
-//   void sensor_data_cb(const sensor_msgs::msg::PointCloud2::SharedPtr pc_msg, const geometry_msgs::msg::PoseStamped::SharedPtr odom_msg) {
-//     RCLCPP_INFO(
-//       this->get_logger(),
-//       "Synchronized data received: PointCloud2 with %d points, Wheel odometry at time %f.",
-//       pc_msg->width * pc_msg->height,
-//       odom_msg->header.stamp.sec + odom_msg->header.stamp.nanosec * 1e-9);
-
-//     // init frame
-//     if (frames_.empty()) {
-//       Frame::Ptr init_frame = std::make_shared<Frame>();
-//       init_frame->cloud = ros_cloud_convert(pc_msg);
-//       init_frame->T_world_robot = Eigen::Isometry3d::Identity();
-//       frames_.emplace_back(init_frame);
-
-//       pr_wheel_T_world_robot_.translation() = Eigen::Vector3d(odom_msg->pose.position.x, odom_msg->pose.position.y, odom_msg->pose.position.z);
-//       pr_wheel_T_world_robot_.linear() =
-//         Eigen::Quaterniond(odom_msg->pose.orientation.w, odom_msg->pose.orientation.x, odom_msg->pose.orientation.y, odom_msg->pose.orientation.z).toRotationMatrix();
-
-//       gtsam::Values initial_estimate;
-//       initial_estimate.insert(X(0), gtsam::Pose3());
-
-//       gtsam::NonlinearFactorGraph graph;
-//       graph.emplace_shared<gtsam::PriorFactor<gtsam::Pose3>>(X(0), gtsam::Pose3(), gtsam::noiseModel::Isotropic::Precision(6, 1e6));
-
-//       gtsam::ISAM2Params isam2_params;
-//       isam2_params.relinearizeThreshold = 0.01;
-//       isam2_params.relinearizeSkip = 1;
-//       isam2_ = std::make_unique<gtsam::ISAM2>(isam2_params);
-//       isam2_->update(graph, initial_estimate);
-
-//       return;
-//     }
-
-//     // wheel odom check
-//     // pr_wheel_T_world_robot_ = cr_wheel_T_world_robot_;
-//     cr_wheel_T_world_robot_.translation() = Eigen::Vector3d(odom_msg->pose.position.x, odom_msg->pose.position.y, odom_msg->pose.position.z);
-//     cr_wheel_T_world_robot_.linear() =
-//       Eigen::Quaterniond(odom_msg->pose.orientation.w, odom_msg->pose.orientation.x, odom_msg->pose.orientation.y, odom_msg->pose.orientation.z).toRotationMatrix();
-//     Eigen::Isometry3d wheel_T_pr_cr = pr_wheel_T_world_robot_.inverse() * cr_wheel_T_world_robot_;
-
-//     if (approx_zero(wheel_T_pr_cr.translation().norm(), 0.1) && approx_zero(Eigen::AngleAxisd(wheel_T_pr_cr.linear()).angle(), 10.0 * M_PI / 180.0)) {
-//       RCLCPP_WARN(this->get_logger(), "Negligible wheel odometry change detected. Skipping frame.");
-//       return;
-//     }
-
-//     RCLCPP_INFO(this->get_logger(), "wheel_T_cr_pr trans : %lf, angle : %lf", wheel_T_pr_cr.translation().norm(), Eigen::AngleAxisd(wheel_T_pr_cr.linear()).angle());
-
-//     pr_wheel_T_world_robot_ = cr_wheel_T_world_robot_;
-
-//     // new frame
-//     Frame::Ptr new_frame = std::make_shared<Frame>();
-//     new_frame->cloud = ros_cloud_convert(pc_msg);
-//     frames_.emplace_back(new_frame);
-//     size_t frame_idx = frames_.size() - 1;
-
-//     gtsam::Values initial_estimate;
-//     gtsam::NonlinearFactorGraph graph;
-
-//     Eigen::Isometry3d init_T_world_robot = frames_[frame_idx - 1]->T_world_robot * wheel_T_pr_cr;
-//     initial_estimate.insert(X(frame_idx), gtsam::Pose3(init_T_world_robot.matrix()));
-
-//     graph.emplace_shared<gtsam::BetweenFactor<gtsam::Pose3>>(X(frame_idx - 1), X(frame_idx), gtsam::Pose3(wheel_T_pr_cr.matrix()),
-//     gtsam::noiseModel::Isotropic::Precision(6, 1.0)); graph.emplace_shared<gtsam_points::IntegratedICPFactor>(X(frame_idx - 1), X(frame_idx), frames_[frame_idx - 1]->cloud,
-//     new_frame->cloud);
-
-//     isam2_->update(graph, initial_estimate);
-//     auto estimated_values = isam2_->calculateEstimate();
-//     for(gtsam::Symbol&& key : estimated_values.keys()) {
-//       size_t idx = key.index();
-//       gtsam::Pose3 est_pose = estimated_values.at<gtsam::Pose3>(key);
-//       frames_[idx]->T_world_robot = Eigen::Isometry3d(est_pose.matrix());
-//     }
-
-//     timer_cb();
-//   }
-// };
-// }  // namespace c107
-
 #pragma once
 
 #include <rclcpp/rclcpp.hpp>
@@ -389,21 +198,11 @@ public:
     estimate_.insert(X(k), gtsam::Pose3(init_T_world_robot.matrix()));
 
     auto between_noise = gtsam::noiseModel::Isotropic::Precision(6, 1.0);
-    // auto between_noise = gtsam::noiseModel::Diagonal::Sigmas((gtsam::Vector(6) << 0.2,
-    //                                                           0.2,
-    //                                                           0.2,  // xyz [m]
-    //                                                           0.1,
-    //                                                           0.1,
-    //                                                           0.1  // rpy [rad]
-    //                                                           )
-    //                                                            .finished());
-    // graph_.emplace_shared<gtsam::BetweenFactor<gtsam::Pose3>>(X(k - 1), X(k), gtsam::Pose3(wheel_T_pr_cr.inverse().matrix()), between_noise);
+    graph_.emplace_shared<gtsam::BetweenFactor<gtsam::Pose3>>(X(k - 1), X(k), gtsam::Pose3(wheel_T_pr_cr.inverse().matrix()), between_noise);
 
     auto icp_factor = gtsam::make_shared<gtsam_points::IntegratedICPFactor>(X(k - 1), X(k), frames_[k - 1]->cloud, new_frame->cloud);
     icp_factor->set_max_correspondence_distance(0.3);
     graph_.add(icp_factor);
-
-    // graph_.emplace_shared<gtsam_points::IntegratedICPFactor>(X(k - 1), X(k), frames_[k - 1]->cloud, new_frame->cloud);
 
     optimize();
     timer_cb();
@@ -432,7 +231,6 @@ public:
     map_cloud_ = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
     RCLCPP_INFO(this->get_logger(), "create map cloud");
     for (size_t i = 0; i < frames_.size(); i++) {
-      // if(i % 5 != 0) continue;  // downsample for visualization
       gtsam_points::PointCloudCPU::Ptr cloud_lidar = frames_[i]->cloud;
       pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_world = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
       cloud_world->points.resize(cloud_lidar->size());
